c087b2fde6b639ea33b8bef2ba3a441dda7fe2da
Bashu Naimi-Roy <bashunaimiroy@gmail.com>
2017-11-16 21:27:27 -0500

    minor config change

--- real-server.js
+++ real-server.js
@@ -11,7 +11,9 @@ const authController = require("./controllas/auth.js")
 const journalDataLoader = require("./lib/dataLoader.js")
 //somehow change this to pool using the env variable?
 const connection = mysql.createConnection(
-`mysql://bf6ef13562bd66:997df2a0@us-cdbr-iron-east-05.cleardb.net/heroku_f43acdfbd975755?reconnect=true`)
+process.env.CLEARDB_DATABASE_URL
+
+)
     .then(connection => InitializeApp(new journalDataLoader(connection)))
 
 //putting this in a function and then only running it once the connection is finished,
